e2bceef37f99c221b62edc9de7bfaa7d7d92c4b5
anviaan <andre.carbajal.vargas@gmail.com>
2024-07-20 18:48:39 -0500

    small change

--- src/main/java/net/anvian/smartinventory/handler/ModKeyBinding.java
+++ src/main/java/net/anvian/smartinventory/handler/ModKeyBinding.java
@@ -1,11 +1,11 @@
-package net.anvian.smartinventory.event;
+package net.anvian.smartinventory.handler;
 
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
-public class KeyInput {
+public class ModKeyBinding {
     public static KeyBinding keyBinding;
 
     public static void register() {
