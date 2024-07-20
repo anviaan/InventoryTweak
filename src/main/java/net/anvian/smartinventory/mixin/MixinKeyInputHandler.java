21d6437d725a3f7b02f86bff523a4dedbbc52648
anviaan <andre.carbajal.vargas@gmail.com>
2024-07-20 00:14:30 -0500

    updating HandledScreen

--- src/main/java/net/anvian/smartinventory/mixin/MixinKeyInputHandler.java
+++ src/main/java/net/anvian/smartinventory/mixin/MixinKeyInputHandler.java
@@ -1,9 +1,12 @@
 package net.anvian.smartinventory.mixin;
 
-import net.anvian.smartinventory.SmartInventory;
 import net.anvian.smartinventory.event.KeyInput;
 import net.anvian.smartinventory.sort.SortInventory;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,7 +17,12 @@ public class MixinKeyInputHandler {
     @Inject(method = "keyPressed", at = @At("HEAD"))
     private void onKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
         if (keyCode == KeyInput.keyBinding.getDefaultKey().getCode()) {
-            SortInventory.sort(SmartInventory.getClient());
+            MinecraftClient client = MinecraftClient.getInstance();
+            Screen screen = client.currentScreen;
+
+            if (screen instanceof InventoryScreen || screen instanceof CreativeInventoryScreen) {
+                SortInventory.sortPlayerInventory(client);
+            }
         }
     }
 }
