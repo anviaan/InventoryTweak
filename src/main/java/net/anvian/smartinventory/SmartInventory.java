429440ca6a3c76ec13d5fc4afc03ce7670f2634d
anviaan <andre.carbajal.vargas@gmail.com>
2024-07-20 18:40:29 -0500

    small change

--- src/main/java/net/anvian/smartinventory/SmartInventory.java
+++ src/main/java/net/anvian/smartinventory/SmartInventory.java
@@ -1,6 +1,6 @@
 package net.anvian.smartinventory;
 
-import net.anvian.smartinventory.event.KeyInput;
+import net.anvian.smartinventory.handler.ModKeyBinding;
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,13 +8,11 @@ import org.slf4j.LoggerFactory;
 public class SmartInventory implements ClientModInitializer {
     public static final String MOD_ID = "smartinventory";
     public static final String MOD_NAME = "Smart Inventory";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
-
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("Hello from " + MOD_NAME + "!");
-
-        KeyInput.register();
+        ModKeyBinding.register();
     }
 }
