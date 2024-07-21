2e3b72f132d6f50f9cc65dddde68bc9e130096c8
anviaan <andre.carbajal.vargas@gmail.com>
2024-07-21 16:13:19 -0500

    fixing player inventory

--- src/main/java/net/anvian/inventorytweaks/slots/PlayerSlots.java
+++ src/main/java/net/anvian/inventorytweaks/slots/PlayerSlots.java
@@ -1,6 +1,7 @@
 package net.anvian.inventorytweaks.slots;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.PlayerScreenHandler;
@@ -9,7 +10,7 @@ import net.minecraft.screen.ScreenHandler;
 public class PlayerSlots {
     public static InventorySlots get() {
         int from, to;
-        if (MinecraftClient.getInstance().currentScreen instanceof InventoryScreen) {
+        if (MinecraftClient.getInstance().currentScreen instanceof InventoryScreen || MinecraftClient.getInstance().currentScreen instanceof CreativeInventoryScreen) {
             from = PlayerScreenHandler.INVENTORY_START;
             to = PlayerScreenHandler.INVENTORY_END;
             return new InventorySlots(from, to, PlayerScreenHandler.OFFHAND_ID);
